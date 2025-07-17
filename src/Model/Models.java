705bb1c684a950b509ff42884ae961144ec683cf
LapTop_Bogdan <bogdanvoinov90@gmail.com>
2025-07-17 13:00:22 +0300

    fix close frame

--- src/Model/Models.java
+++ src/Model/Models.java
@@ -1,12 +1,36 @@
 package Model;
 
 import Simulation.DrawFunc;
+import Simulation.SimulationRunner;
 import Simulation.ThawingFunc;
 import Structures.PointsParameters;
 
-public class Models {
+import java.util.Objects;
+
+public  class Models {
     public static final int xOffset = 70;
     public static final int yOffset = 70;
+    public int weight;
+    public int height;
+    static final int border = 15;
+    public static final int radius = 3;
+    public ThawingFunc thawing_func;
+    public DrawFunc draw_func;
+    public PointsParameters[] points_parameters;
+    String description;
+
+    protected Models(Object thawing_func, DrawFunc draw_func, PointsParameters[] points_parameters, int weight, int height, String description) {
+        this.thawing_func = (ThawingFunc) thawing_func;
+        this.draw_func = draw_func;
+        this.points_parameters = points_parameters;
+        this.weight = weight;
+        this.height = height;
+        this.description = description;
+    }
+
+    public Models() {
+
+    }
 
     public void setWeight(int weight) {
         this.weight = weight;
@@ -16,20 +40,11 @@ public class Models {
         this.height = height;
     }
 
-    public int weight;
-    public int height;
-    static final int border = 15;
-    public static final int radius = 3;
-    public ThawingFunc thawing_func;
-    public DrawFunc draw_func;
-    public PointsParameters[] points_parameters;
-
-    protected Models(ThawingFunc thawing_func, DrawFunc draw_func, PointsParameters[] points_parameters, int weight, int height) {
-        this.thawing_func = thawing_func;
-        this.draw_func = draw_func;
-        this.points_parameters = points_parameters;
-        this.weight = weight;
-        this.height = height;
+    public String getDescription() {
+        return description;
+    }
+    public void run(){
+        new SimulationRunner(this);
     }
 }
 
--- src/mainMofPinaG.java
+++ src/mainMofPinaG.java
@@ -1,9 +1,13 @@
+import Graphics.ChoiceFrame;
+import Model.Models;
 import Model.StandardModel;
-
+class ApplicationRunner{
+     public static Models[] models =  new Models[]{(Models) new StandardModel()};
+}
 
 public class mainMofPinaG {
     public static void main(String[] args) {
-
-        new StandardModel();
+        new ChoiceFrame(ApplicationRunner.models);
+//        new StandardModel().run();
     }
 }
